05f259e58f5b7e1767a9f8fa7a0450a7bb72e180
Giovanna <giovannad1clonius@gmail.com>
2023-05-21 19:14:14 -0300

    get url by id

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -9,13 +9,26 @@ export async function postUrlShorten(req, res) {
 
     try {
         const tokenExists = await db.query(`SELECT * FROM sessions WHERE token=$1;`, [token]);
-        if(!tokenExists) return res.sendStatus(401);
+        if (!tokenExists) return res.sendStatus(401);
 
         const shortenUrl = nanoid.apply(url);
-        await db.query(`INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3);`, [tokenExists.rows[0].userId, url, shortenUrl]);
+        await db.query(`INSERT INTO urls ("userId", "shortUrl") VALUES ($1, $2);`, [tokenExists.rows[0].userId, shortenUrl]);
 
         res.status(201).send(shortenUrl);
 
+    } catch (err) {
+        return res.status(500).send(err.message);
+    };
+};
+
+export async function getUrlById(req, res) {
+    const { id } = req.params;
+
+    try {   
+        const body = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE id=$1`, [id]);
+        if(body.rows.length === 0) return res.sendStatus(404);
+
+        return res.status(200).send(body.rows[0]);
     } catch (err) {
         return res.status(500).send(err.message);
     };
--- src/routers/urls.router.js
+++ src/routers/urls.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { urlValidation } from "../middlewares/urls.validation.js";
-import { postUrlShorten } from "../controllers/urls.controllers.js";
+import { getUrlById, postUrlShorten } from "../controllers/urls.controllers.js";
 
 const router = Router();
 
 router.post("/urls/shorten", urlValidation, postUrlShorten);
-router.get("/urls/:id");
+router.get("/urls/:id", getUrlById);
 router.get("/urls/open/:shortUrl");
 router.delete("/urls/:id");
 
