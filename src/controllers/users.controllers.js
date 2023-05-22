56f6bb38647534a04c792a309dcdb8284bfcdb85
Giovanna <giovannad1clonius@gmail.com>
2023-05-21 23:14:53 -0300

    get user

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -37,3 +37,37 @@ export async function signup(req, res) {
         return res.status(500).send(err.message);
     };
 };
+
+export async function getUser (req, res){
+    const { authorization } = req.headers;
+    if (!authorization) return res.sendStatus(401);
+    const token = authorization.replace("Bearer ", "");
+
+    try {
+        const tokenExists = await db.query(`SELECT * FROM sessions WHERE token=$1;`, [token]);
+        if (tokenExists.rows.length === 0) return res.sendStatus(401);
+
+        const users = await db.query(`
+        SELECT users.*, SUM("visitCount") AS "visitCount" 
+        FROM users 
+        JOIN sessions ON users.id = sessions."userId" 
+        JOIN urls ON users.id = urls."userId" 
+        WHERE sessions.token = $1 
+        GROUP BY users.id;`, [token]);
+        const user = users.rows[0];
+        const id = users.rows[0].id;
+
+        const urls = await db.query(`SELECT urls.id, "shortUrl", "url", "visitCount" FROM urls JOIN users ON urls."userId" = users.id WHERE urls."userId" = $1;`, [id])
+    
+        const body = {
+            id: id,
+	        name: user.name,
+            visitCount: user.visitCount,
+            shortenedUrls: urls.rows
+        }
+
+        return res.status(200).send(body);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    };
+};
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { signinValidation, signupValidation } from "../middlewares/users.validation.js";
-import { signin, signup } from "../controllers/users.controllers.js";
+import { getUser, signin, signup } from "../controllers/users.controllers.js";
 
 const router = Router();
 
 router.post("/signup", signupValidation, signup);
 router.post("/signin", signinValidation, signin);
-router.get("/users/me");
+router.get("/users/me", getUser);
 router.get("/ranking");
 
 export default router;
